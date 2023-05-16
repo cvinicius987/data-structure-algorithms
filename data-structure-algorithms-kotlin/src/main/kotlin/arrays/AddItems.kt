41825fdd0d7d862fb5da817b0d9b12142938f6b6
Caio Vinicius <caio.vinicius@ifood.com.br>
2023-05-16 17:34:24 -0300

    Refactor on arr add logic

--- data-structure-algorithms-kotlin/src/main/kotlin/arrays/AddItems.kt
+++ data-structure-algorithms-kotlin/src/main/kotlin/arrays/AddItems.kt
@@ -2,35 +2,27 @@ package arrays
 
 fun main() {
     val array = arrayOf("Java", "Kotlin", "Golang", "Rust", "C++")
-    val newArray = addLinearPosition(array, "JavaScript", 1)
+    val newArray = addLinearPosition(array, "JavaScript", 0)
 
     println("Original: ${array.contentToString()}")
     println("New: ${newArray.contentToString()}")
 }
 
-fun addLinearPosition(arr:Array<String>, item:String, position:Int):Array<String>{
+fun addLinearPosition(arr: Array<String>, item: String, position: Int): Array<String> {
+    require(position >= 0 && position <= arr.size) { "Position is out of bounds" }
 
-    if(position >= arr.size)
-        throw ArrayIndexOutOfBoundsException("Position is bigger than arr size")
+    val newArray = Array(arr.size + 1) { "" }
+    var newIndex = 0
 
-    val newArray = Array(arr.size + 1){r -> r.toString()}
-    newArray[arr.size] = arr[arr.size - 1]
-
-    var nextData:String? = null
-
-    for ((currentIndex, current) in arr.withIndex()){
-        if(currentIndex == position && nextData == null) {
-            newArray[currentIndex] = item
-            nextData = current
-        }else{
-            if (nextData != null) {
-                newArray[currentIndex] = nextData
-                nextData = current
-                continue
-            }
-
-            newArray[currentIndex] = current
+    for ((currentIndex, current) in arr.withIndex()) {
+        if (currentIndex == position) {
+            newArray[newIndex++] = item
         }
+        newArray[newIndex++] = current
+    }
+
+    if (position == arr.size) {
+        newArray[newIndex] = item
     }
 
     return newArray
